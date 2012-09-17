f5ff85f0613ebeb9f3c6642a9c57ae88459eb938
Tim Shen <innocentsty@gmail.com>
2012-09-17 20:14:30 +0800

    notification subject

--- notification.go
+++ notification.go
@@ -113,7 +113,7 @@ func initNotification() {
 
 func send(to, subject, msg string) {
 	logger.Println("notification:", "SendMail:", msg, "for", to)
-	if err := smtp.SendMail(config["SMTPAddr"], mailAuth, "no-reply@"+config["Domain"], []string{to}, []byte("To: "+to+"\nSubject: "+subject+"\nContent-Type: text/html; charset=\"UTF-8\"\n"+msg)); err != nil {
+	if err := smtp.SendMail(config["SMTPAddr"], mailAuth, "no-reply@"+config["Domain"], []string{to}, []byte("To: "+to+"\nSubject: ["+config["ServerName"]+"] "+subject+"\nContent-Type: text/html; charset=\"UTF-8\"\n"+msg)); err != nil {
 		logger.Println("SendMail:", err.Error())
 	}
 }
